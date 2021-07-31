78660f1b0331b4bb9025d38899d950460e8d77ba
iui <iui@iui.com>
2021-07-31 09:28:32 +0800

    naive

---
+++ leetcode/editor/cn/100278-XuanZhuanShuZuDeZuiXiaoShuZiLcof.cpp
@@ -0,0 +1,47 @@
+//把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。例如，数组 [3,4,5,1,2
+//] 为 [1,2,3,4,5] 的一个旋转，该数组的最小值为1。 
+//
+// 示例 1： 
+//
+// 输入：[3,4,5,1,2]
+//输出：1
+// 
+//
+// 示例 2： 
+//
+// 输入：[2,2,2,0,1]
+//输出：0
+// 
+//
+// 注意：本题与主站 154 题相同：https://leetcode-cn.com/problems/find-minimum-in-rotated-sor
+//ted-array-ii/ 
+// Related Topics 数组 二分查找 
+// 👍 370 👎 0
+
+
+#include "include/headers.h"
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    int minArray(vector<int> &numbers) {
+        int min = numbers.at(numbers.size()-1);
+        for (auto n:numbers) {
+            if (n < min) {
+                min = n;
+            }
+        }
+        return min;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main() {
+    Solution s;
+    vector<int> data{7, 1, 5, 3, 6, 4};
+    auto res = s.minArray(data);
+    cout << res << endl;
+}
