03adcdecf8e1376e9bf306d74dee16f1b817a2fc
iui <iui@iui.com>
2021-08-22 12:14:22 +0800

    recur isSymmetric

---
+++ leetcode/editor/cn/100289-DuiChengDeErChaShuLcof.cpp
@@ -0,0 +1,78 @@
+//请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。 
+//
+// 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。 
+//
+// 1 
+// / \ 
+// 2 2 
+// / \ / \ 
+//3 4 4 3 
+//但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的: 
+//
+// 1 
+// / \ 
+// 2 2 
+// \ \ 
+// 3 3 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：root = [1,2,2,3,4,4,3]
+//输出：true
+// 
+//
+// 示例 2： 
+//
+// 输入：root = [1,2,2,null,3,null,3]
+//输出：false 
+//
+// 
+//
+// 限制： 
+//
+// 0 <= 节点个数 <= 1000 
+//
+// 注意：本题与主站 101 题相同：https://leetcode-cn.com/problems/symmetric-tree/ 
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 
+// 👍 224 👎 0
+
+
+#include "include/headers.h"
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode *root) {
+        if (root == nullptr) {
+            return true;
+        } else {
+            return recur(root->left, root->right);
+        }
+    }
+
+    bool recur(TreeNode *l, TreeNode *r) {
+        if (!l && !r) return true;
+        if (!l || !r || l->val != r->val) return false;
+        return recur(l->left, r->right) && recur(l->right, r->left);
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main() {
+    Solution s;
+    cout << s << endl;
+}
