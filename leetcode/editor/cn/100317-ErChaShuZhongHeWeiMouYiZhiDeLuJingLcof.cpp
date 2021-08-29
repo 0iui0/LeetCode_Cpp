386cbc20ba32e71098cae69f6e1d27b8253099d3
iui <iui@iui.com>
2021-08-29 18:28:38 +0800

    pathSum:cunt down until (sum==0 && left=null && right==null),push path;recur left and right;hit:before backpop remove cur node (it will return but sum!=0)

---
+++ leetcode/editor/cn/100317-ErChaShuZhongHeWeiMouYiZhiDeLuJingLcof.cpp
@@ -0,0 +1,84 @@
+//输入一棵二叉树和一个整数，打印出二叉树中节点值的和为输入整数的所有路径。从树的根节点开始往下一直到叶节点所经过的节点形成一条路径。 
+//
+// 
+//
+// 示例: 
+//给定如下二叉树，以及目标和 target = 22， 
+//
+// 
+//              5
+//             / \
+//            4   8
+//           /   / \
+//          11  13  4
+//         /  \    / \
+//        7    2  5   1
+// 
+//
+// 返回: 
+//
+// 
+//[
+//   [5,4,11,2],
+//   [5,8,4,5]
+//]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 节点总数 <= 10000 
+// 
+//
+// 注意：本题与主站 113 题相同：https://leetcode-cn.com/problems/path-sum-ii/ 
+// Related Topics 树 深度优先搜索 回溯 二叉树 
+// 👍 224 👎 0
+
+
+#include "include/headers.h"
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    vector<vector<int>> ans;
+    vector<int> path;
+
+    void dfs(TreeNode *root, int sum) {
+        if (!root) return;
+        path.push_back(root->val);
+        sum -= root->val;
+        if (!sum && !root->left && !root->right)
+            ans.push_back(path);
+        dfs(root->left, sum), dfs(root->right, sum);
+        // 向上回溯前，需要将当前节点从路径 path 中删除
+        path.pop_back();
+    }
+
+public:
+    vector<vector<int>> pathSum(TreeNode *root, int target) {
+        dfs(root, target);
+        return ans;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main() {
+    Solution s;
+    cout << s << endl;
+}
