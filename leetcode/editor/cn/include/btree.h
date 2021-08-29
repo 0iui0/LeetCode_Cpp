830abb0eebdf2cffb4bc4af486db100a686bedc4
iui <iui@iui.com>
2021-08-29 18:56:55 +0800

    add node class

--- leetcode/editor/cn/include/btree.h
+++ leetcode/editor/cn/include/btree.h
@@ -52,6 +52,26 @@ public:
 private:
     TreeNode *deserialize(std::queue<int> &in);
 };
+    class Node {
+    public:
+        int val;
+        Node *left;
+        Node *right;
+
+        Node() {}
+
+        Node(int _val) {
+            val = _val;
+            left = NULL;
+            right = NULL;
+        }
+
+        Node(int _val, Node *_left, Node *_right) {
+            val = _val;
+            left = _left;
+            right = _right;
+        }
+    };
 } // namespace LeetCode
 
 #endif //!_BTREE_H_
