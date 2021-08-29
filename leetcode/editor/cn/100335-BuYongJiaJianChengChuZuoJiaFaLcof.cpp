e2f8923b47fe94f0c87d29df0bf52180f3fa77c1
iui <iui@iui.com>
2021-08-29 15:37:20 +0800

    add:a+b=(a^b+(a&b)<<1)

---
+++ leetcode/editor/cn/100335-BuYongJiaJianChengChuZuoJiaFaLcof.cpp
@@ -0,0 +1,41 @@
+//写一个函数，求两个整数之和，要求在函数体内不得使用 “+”、“-”、“*”、“/” 四则运算符号。 
+//
+// 
+//
+// 示例: 
+//
+// 输入: a = 1, b = 1
+//输出: 2 
+//
+// 
+//
+// 提示： 
+//
+// 
+// a, b 均可能是负数或 0 
+// 结果不会溢出 32 位整数 
+// 
+// Related Topics 位运算 数学 
+// 👍 206 👎 0
+
+
+#include "include/headers.h"
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    int add(int a, int b) {
+        if (!b) return a;
+        //sum=无进位和+进位；异或+与左移
+        return add(a ^ b, (a & b) << 1);
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main() {
+    Solution s;
+    cout << s.add(3, 2) << endl;
+}
