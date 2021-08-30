f9a24dbdb731d50a9bb759e6f5c2e1aab9dad38e
iui <iui@iui.com>
2021-08-30 10:48:13 +0800

    find only two single Num,xy ;xy=XoR... mask&xy find first diff bit, diff bit will split array into two array

---
+++ leetcode/editor/cn/100320-ShuZuZhongShuZiChuXianDeCiShuLcof.cpp
@@ -0,0 +1,53 @@
+//一个整型数组 nums 里除两个数字之外，其他数字都出现了两次。请写程序找出这两个只出现一次的数字。要求时间复杂度是O(n)，空间复杂度是O(1)。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：nums = [4,1,4,6]
+//输出：[1,6] 或 [6,1]
+// 
+//
+// 示例 2： 
+//
+// 输入：nums = [1,2,10,4,1,4,3,3]
+//输出：[2,10] 或 [10,2] 
+//
+// 
+//
+// 限制： 
+//
+// 
+// 2 <= nums.length <= 10000 
+// 
+//
+// 
+// Related Topics 位运算 数组 
+// 👍 454 👎 0
+
+
+#include "include/headers.h"
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    vector<int> singleNumbers(vector<int> &nums) {
+        int x = 0, y = 0, mask = 1, xy = 0;
+        for (int n:nums) xy = xy ^ n;
+        while (xy & mask) mask = mask << 1;
+        for (int n:nums) {
+            if (n & mask) x = x ^ n;
+            else y = y ^ n;
+        }
+        return vector<int>{x, y};
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main() {
+    Solution s;
+    cout << s << endl;
+}
