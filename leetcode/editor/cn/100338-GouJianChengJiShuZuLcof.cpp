b84a6bed97bfc2c339e132d9e6bc4ba111a0f09a
iui <iui@iui.com>
2021-08-31 23:08:22 +0800

    format

--- leetcode/editor/cn/100338-GouJianChengJiShuZuLcof.cpp
+++ leetcode/editor/cn/100338-GouJianChengJiShuZuLcof.cpp
@@ -32,7 +32,7 @@ public:
         if (a.empty()) return {};
         vector<int> b(a.size(), 1);
         for (int i = 1; i < a.size(); ++i)
-            b[i] = b[i - 1] * a[i-1];
+            b[i] = b[i - 1] * a[i - 1];
         int tmp = 1;
         for (int i = a.size() - 2; i >= 0; --i) {
             tmp *= a[i + 1];
